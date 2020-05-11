440cb273bf4ae56dbd1180a346515ac96a0bd481
Charlie Thao <cthao309@gmail.com>
2020-05-11 16:35:57 -0700

    WIP: set up the express server for the proxy for FEC

---
+++ server/proxyServer.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = 8000;
+
+app.use(express.static(path.join(__dirname, '../client')));
+
+app.listen(PORT, () => {
+  console.log(`proxy server is listening on http://localhost:${PORT}`);
+})
