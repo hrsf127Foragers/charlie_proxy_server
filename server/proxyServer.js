4dd547f5a840dc519d8cf8cdc49017c1d93bc7af
Charlie Thao <cthao309@gmail.com>
2020-05-15 23:16:32 -0700

    created re-route to serve micro service

--- server/proxyServer.js
+++ server/proxyServer.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const path = require('path');
 
 const app = express();
@@ -6,6 +7,17 @@ const app = express();
 const PORT = 8000;
 
 app.use(express.static(path.join(__dirname, '../client')));
+
+app.get('/api/:restaurant_id/reviews', (req, res) => {
+  console.log('rest id => ', req.params.restaurant_id)
+  axios.get(`http://54.183.220.133:3001/api/${req.params.restaurant_id}/reviews`)
+    .then(({data}) => {
+      res.status(200).send(data)
+    })
+    .catch(err => {
+      res.status(500).send(err)
+    })
+})
 
 app.listen(PORT, () => {
   console.log(`proxy server is listening on http://54.183.247.65:${PORT}`);
