5126a6f04956e6d14ea26c0211b3d77535960344
Charlie Thao <cthao309@gmail.com>
2020-05-15 21:32:25 -0700

    added aws ip onto proxy server

--- server/proxyServer.js
+++ server/proxyServer.js
@@ -8,5 +8,5 @@ const PORT = 8000;
 app.use(express.static(path.join(__dirname, '../client')));
 
 app.listen(PORT, () => {
-  console.log(`proxy server is listening on http://localhost:${PORT}`);
+  console.log(`proxy server is listening on http://54.183.247.65:${PORT}`);
 })
